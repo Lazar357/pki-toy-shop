bd308f6e7ec9266e02000f857ca4b7f24452d95e
Lazar357 <lazar.danic.23@singimail.rs>
2025-10-06 20:58:26 +0200

    Inital commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('pki-toy-shop');
+  protected readonly title = signal('Lazar');
 }
